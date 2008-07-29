9451318546658a92207dba9755a798fd1ad5cd9a
snslabs <snslabs@abd6c467-2953-0410-b3f5-8f14b411ad03>
2008-07-29 13:32:07 +0000

    initial import from old repository

---
+++ trunk/README.txt
@@ -0,0 +1,6 @@
+To run build in your environment, copy 
+build.properties -> build-my.properties
+senEnv.cmd -> setEnv-my.cmd
+
+and make apropriate changes to these files.
+Please note, that build-my.properties and setEnv-my.cmd should not be commited to SVN as they are environment depended.
---
+++ trunk/src/com/itci/Schema2svn.java
@@ -0,0 +1,58 @@
+package com.itci;
+
+import com.itci.schema2svn.DDLFetcher;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.File;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+
+public class Schema2svn {
+    private static final String DATE_PATTERN = "yyyy.MM.dd.HH.mm.ss.SSS";
+    public static final SimpleDateFormat SDF = new SimpleDateFormat(DATE_PATTERN);
+    public static void main(String[] args) {
+        if(args ==null || args.length == 0 || "-h".equalsIgnoreCase(args[0]) || "-help".equalsIgnoreCase(args[0])){
+            System.out.println("Usage:\n" +
+                    "Schema2svn {database_connection} {base_url} [-d2008.07.06.13.24.46.345] {-sSCHEMALIST}\n" +
+                    "If no base_url specified it will use current directory.\n" +
+                    "If base_url is specified, then it will create a temp directory, checkout svn from that URL, " +
+                    "then use that directory as the location for schema files.");
+            return;
+        }
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        Date updateAfter = null;
+        if(args.length>2){
+            System.out.println(args[2]);
+            try {
+                updateAfter = SDF.parse(args[2].replaceAll("-d",""));
+            }
+            catch (ParseException e) {
+                System.out.println("Incorrect date format!\nUse " +DATE_PATTERN);
+                return;
+            }
+
+        }
+        
+        List schemaList = new ArrayList();
+        if(args.length > 3){
+            String schemas = args[3].replaceAll("-s"," ");
+            schemaList.addAll(Arrays.asList( schemas.split("\\s*,\\s*") ));
+        }
+
+        List objectTypeList = new ArrayList();
+        objectTypeList.add("TABLE");
+        objectTypeList.add("VIEW");
+        objectTypeList.add("PROCEDURE");
+        objectTypeList.add("FUNCTION");
+        objectTypeList.add("SEQUENCE");
+        objectTypeList.add("PACKAGE");
+        objectTypeList.add("PACKAGE_BODY");
+        final DDLFetcher fetcher = new DDLFetcher(schemaList, objectTypeList, args[0] , new File(args[1]), updateAfter);
+        fetcher.fetch();
+    }
+}
