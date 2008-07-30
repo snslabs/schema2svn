14b7b527dad68724c7af30d4abfc0ed859897d70
snslabs <snslabs@abd6c467-2953-0410-b3f5-8f14b411ad03>
2008-07-30 15:35:00 +0000

    fix issue when if there are no GRANTS for object - everything crashes

--- trunk/src/com/itci/schema2svn/DDLFetcher.java
+++ trunk/src/com/itci/schema2svn/DDLFetcher.java
@@ -7,14 +7,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
-import java.sql.Clob;
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Timestamp;
+import java.sql.*;
 import java.util.List;
 
 public class DDLFetcher {
@@ -26,9 +19,10 @@ public class DDLFetcher {
     private Connection connection;
     private PreparedStatement objectNamesStatement;
     private PreparedStatement tableMetadataStatement;
-    private PreparedStatement metadataStatement;
+    private PreparedStatement ddlStatement;
     private static final String OBJECT_NAMES_SQL = "select OWNER, OBJECT_NAME, OBJECT_TYPE, LAST_DDL_TIME from all_objects " +
             "where owner = ? and object_type = ?";
+    private PreparedStatement grantsStatement;
 
     public DDLFetcher(List schemaList, List objectTypes, String connectionUrl, File destinationDirectory, java.util.Date updatedAfter) {
         this.schemaList = schemaList;
@@ -47,15 +41,15 @@ public class DDLFetcher {
             e.printStackTrace();
             return null;
         }
-        long maxTimestamp = updatedAfter==null?0:updatedAfter.getTime();
+        long maxTimestamp = updatedAfter == null ? 0 : updatedAfter.getTime();
         try {
             connection = DriverManager.getConnection(connectionUrl);
 
             initStatements();
-            System.out.println("Checking modifications since " + (updatedAfter!=null?updatedAfter.toString():"0"));
+            System.out.println("Checking modifications since " + (updatedAfter != null ? updatedAfter.toString() : "0"));
             System.out.println("Schemas: " + schemaList);
             int modificationCounter = 0;
-            
+
             for (int i = 0; i < schemaList.size(); i++) {
                 String schemaName = ((String) schemaList.get(i)).trim();
                 File schemaDirectory = new File(destinationDirectory, schemaName.toUpperCase());
@@ -72,35 +66,35 @@ public class DDLFetcher {
 
                     objectNamesStatement.setString(1, schemaName);
                     objectNamesStatement.setString(2, objectType.replaceAll("_", " "));
-                    if(updatedAfter!= null){
+                    if (updatedAfter != null) {
                         objectNamesStatement.setTimestamp(3, new Timestamp(updatedAfter.getTime()));
                     }
-                    try{
+                    try {
                         final ResultSet rs = objectNamesStatement.executeQuery();
                         while (rs.next()) {
-                            System.out.println(rs.getString(1) + " : " + rs.getString(2) + " : " + rs.getString(3)+":"+rs.getTimestamp(4));
+                            System.out.println(rs.getString(1) + " : " + rs.getString(2) + " : " + rs.getString(3) + ":" + rs.getTimestamp(4));
                             getAndStoreDDL(typeDirectory, rs.getString(1), rs.getString(2), rs.getString(3), rs.getTimestamp(4));
-                            if(rs.getTimestamp(4).getTime()>maxTimestamp){
+                            if (rs.getTimestamp(4).getTime() > maxTimestamp) {
                                 maxTimestamp = rs.getTimestamp(4).getTime();
                             }
                             modificationCounter++;
                         }
                         rs.close();
                     }
-                    catch(SQLException e){
+                    catch (SQLException e) {
                         System.out.println("ERROR " + e.getMessage());
                     }
 
                 }
 
             }
-            System.out.println("Detected " + modificationCounter +" change(s)");
+            System.out.println("Detected " + modificationCounter + " change(s)");
             final String revision = Schema2svn.SDF.format(new Date(maxTimestamp));
             System.out.println("Revision timestamp (max(last_ddl_time)) = " + revision);
 
             objectNamesStatement.close();
-            
-            return new Result(revision, modificationCounter); 
+
+            return new Result(revision, modificationCounter);
         }
         catch (Exception e) {
             e.printStackTrace();
@@ -122,14 +116,18 @@ public class DDLFetcher {
         if (updatedAfter == null) {
             objectNamesStatement = connection.prepareStatement(
                     OBJECT_NAMES_SQL);
-        }
-        else {
+        } else {
             objectNamesStatement = connection.prepareStatement(
                     OBJECT_NAMES_SQL + " and LAST_DDL_TIME > ?");
         }
-        metadataStatement = connection.prepareStatement(
+        ddlStatement = connection.prepareStatement(
                 "select " +
                         "dbms_metadata.get_ddl(?,?,?), dbms_metadata.get_dependent_ddl('OBJECT_GRANT',?,?) " +
+                        "from dual"
+        );
+        grantsStatement = connection.prepareStatement(
+                "select " +
+                        "dbms_metadata.get_dependent_ddl('OBJECT_GRANT',?,?) " +
                         "from dual"
         );
     }
@@ -137,25 +135,51 @@ public class DDLFetcher {
     char[] buff = new char[1024];
 
     private void getAndStoreDDL(File typeDirectory, String schemaName, String objectName, String objectType, Timestamp timestamp) throws SQLException, IOException {
-        metadataStatement.setString(1, objectType.replaceAll("\\s", "_"));
-        metadataStatement.setString(2, objectName);
-        metadataStatement.setString(3, schemaName);
-        metadataStatement.setString(4, objectName);
-        metadataStatement.setString(5, schemaName);
+        ddlStatement.setString(1, objectType.replaceAll("\\s", "_"));
+        ddlStatement.setString(2, objectName);
+        ddlStatement.setString(3, schemaName);
+        grantsStatement.setString(1, objectName);
+        grantsStatement.setString(2, schemaName);
 
-        final ResultSet rs = metadataStatement.executeQuery();
-        if (rs.next()) {
-            final File file = new File(typeDirectory, objectName.toUpperCase() + ".sql");
-            final FileWriter fw = new FileWriter(file);
-            fw.write("-- LAST DDL : " + Schema2svn.SDF.format(timestamp) +"\n");
-            saveClobToWriter(rs.getClob(1), fw, objectType.equals("TABLE") ? ScriptProcessor.TABLE : null);
-            fw.write("\n/");
-            fw.write("\n\n-- GRANTS\n");
-            saveClobToWriter(rs.getClob(2), fw, ScriptProcessor.GRANTS);
-            fw.flush();
-            fw.close();
+        final File file = new File(typeDirectory, objectName.toUpperCase() + ".sql");
+        final FileWriter fw = new FileWriter(file);
+        ResultSet rs = null;
+        try {
+            rs = ddlStatement.executeQuery();
+            fw.write("-- LAST DDL : " + Schema2svn.SDF.format(timestamp) + "\n");
+            if (rs.next()) {
+                saveClobToWriter(rs.getClob(1), fw, objectType.equals("TABLE") ? ScriptProcessor.TABLE : null);
+                fw.write("\n/");
+            }
         }
-        rs.close();
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        finally {
+            if (rs != null) {
+                rs.close();
+            }
+        }
+
+        fw.write("\n\n-- GRANTS\n");
+        try {
+            rs = grantsStatement.executeQuery();
+            if (rs.next()) {
+                saveClobToWriter(rs.getClob(1), fw, ScriptProcessor.GRANTS);
+            }
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        finally {
+            if (rs != null) {
+                rs.close();
+            }
+        }
+
+        fw.flush();
+        fw.close();
+
 
     }
 
@@ -164,7 +188,7 @@ public class DDLFetcher {
     }
 
     private void saveClobToWriter(Clob clob, FileWriter fw, ScriptProcessor scriptProcessor) throws IOException, SQLException {
-        try{
+        try {
             final Reader characterStream = clob.getCharacterStream();
             char[] buff = new char[1024];
             int cnt;
@@ -172,8 +196,7 @@ public class DDLFetcher {
                 while ((cnt = characterStream.read(buff)) != -1) {
                     fw.write(buff, 0, cnt);
                 }
-            }
-            else {
+            } else {
                 StringBuffer sb = new StringBuffer();
                 while ((cnt = characterStream.read(buff)) != -1) {
                     sb.append(new String(buff, 0, cnt));
@@ -182,11 +205,12 @@ public class DDLFetcher {
 
             }
         }
-        catch(Exception e){
+        catch (Exception e) {
             System.out.println("Nothing in clob");
         }
     }
-    public static class Result{
+
+    public static class Result {
         private String revision;
         private int modificationCounter;
 
